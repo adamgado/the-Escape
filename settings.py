6e17e6d7efaa39edea924b6a85b18b3d89c814fd
adamgado <65188827+adamgado@users.noreply.github.com>
2024-09-18 22:18:52 +0300

    settings constants comments

--- settings.py
+++ settings.py
@@ -4,6 +4,7 @@ import math
 RES = WIDTH, HEIGHT = 1600, 900
 FPS = 60
 
+"""Player attribute constants"""
 PLAYER_POSITION = 1.5, 5
 PLAYER_ANGLE = 0
 PLAYER_SPEED = 0.004
@@ -11,13 +12,14 @@ PLAYER_ROTATION = 0.002
 PLAYER_SIZE = 60
 MAX_HEALTH = 100
 
+"""mouse controls constants"""
 MOUSE_SENS = 0.0002
 MOUSE_MAX = 40
 BORDER_LEFT = 100
 BORDER_RIGHT = WIDTH - BORDER_LEFT
 
+"""raycasting and 3d projection constants"""
 FLOOR = (30, 30, 30)
-
 FOV = math.pi / 2
 RAYSNUM = WIDTH // 2
 RAYS_ANGLE = FOV / RAYSNUM
@@ -25,5 +27,4 @@ DISTANCE = 20
 HALF_RAYSNUM = RAYSNUM // 2
 SCREEN = (WIDTH // 2) / math.tan(FOV / 2)
 SCALE = WIDTH // RAYSNUM
-
 TEXTURE = 256
